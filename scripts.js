26d8e210c7ea6e9a5abec6272b9598107cae0f20
tomatohe <124441381+tomatohe@users.noreply.github.com>
2023-12-22 13:31:18 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,26 +1,30 @@
-// 设置开始日期为固定值（例如：2023年1月1日）
-var defaultStartDate = new Date('2020-01-01');
-document.getElementById('startDate').valueAsDate = defaultStartDate;
 
-// 设置结束日期为今天
-var today = new Date();
-var day = today.getDate();
-var month = today.getMonth() + 1; // 注意月份是从0开始计数的，所以要加1
-var year = today.getFullYear();
-var formattedToday = year + '-' + (month < 10 ? '0' : '') + month + '-' + (day < 10 ? '0' : '') + day;
+<label for="startDate">Start Date:</label>
+<input type="date" id="startDate">
 
-document.getElementById('endDate').value = formattedToday;
+<label for="endDate">End Date:</label>
+<input type="date" id="endDate">
 
-// 计算函数保持不变
-function calculateDays() {
+<button onclick="calculateDays()">Calculate Days</button>
+
+<p id="result"></p>
+
+<script>
+  // 设置开始日期为固定值（例如：2023年1月1日）
+  var defaultStartDate = new Date('2020-01-01');
+  document.getElementById('startDate').valueAsDate = defaultStartDate;
+
+  // 计算函数保持不变
+  function calculateDays() {
     var startDate = new Date(document.getElementById('startDate').value);
-    var endDate = new Date(document.getElementById('endDate').value);
+    var endDate = new Date();  // 设置结束日期为今天
 
     // Check if both dates are valid
     if (isNaN(startDate) || isNaN(endDate)) {
-        alert('Please enter valid dates.');
-        return;
+      alert('Please enter valid dates.');
+      return;
     }
+
     var timeDiff = endDate - startDate; 
     var secondsDiff = Math.floor(timeDiff / 1000); 
     var days = Math.floor(secondsDiff / (3600 * 24)); 
@@ -29,10 +33,6 @@ function calculateDays() {
     var seconds = secondsDiff % 60; 
     
     document.getElementById('result').innerHTML = 'We have been in love for: ' + days + ' days, ' + hours + ' hours, '+ minutes + ' minutes, '+ seconds + ' seconds ^_^'; 
+  }
+</script>
 
-    console.log('Days:', days);
-    console.log('Hours:', hours);
-    console.log('Minutes:', minutes);
-    console.log('Seconds:', seconds);
-
-}
